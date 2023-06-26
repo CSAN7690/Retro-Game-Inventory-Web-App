2851d91a8077cbdbeac70115a9ea127faf00d69d
christophersanchez <christophersanchez@pursuit.org>
2023-06-26 11:43:43 -0400

    Updated script.js with list of games, prices & inStock

---
+++ script.js
@@ -0,0 +1,42 @@
+const games = [
+    {
+        name: "Pokemon Red",
+        price: 20,
+        inStock: 5,
+    },
+    {
+        name: "Pokemon Yellow",
+        price: 20,
+        inStock: 2,
+    },
+    {
+        name: "Metroid",
+        price: 15,
+        inStock: 8,
+    },
+    {
+        name: "Donkey Kong",
+        price: 10,
+        inStock: 3,
+    },
+    {
+        name: "Mega Man V",
+        price: 18,
+        inStock: 6,
+    },
+    {
+        name: "Kirby's Dream Land",
+        price: 12,
+        inStock: 4,
+    },
+    {
+        name: "Tetris",
+        price: 10,
+        inStock: 7,
+    },
+    {
+        name: "Super Mario Land",
+        price: 15,
+        inStock: 5,
+    },
+];
